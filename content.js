7beddfcc320d07dcc05b226b155dc7e1635da2f5
Scottw1105 <scottw1105@gmail.com>
2025-01-07 14:23:04 -0600

    updated commit.

--- content.js
+++ content.js
@@ -20,7 +20,7 @@ async function getLineups(tourney_info, index) {
 
 async function getTourneyInfo() {
     var tourney_info = [];
-    document.querySelector(".styles__infoIcon__i2XtS").click();
+    document.querySelector(".styles__infoIcon__Ac3ZD").click();
     await sleep(1000);
 
     var tourney_name = document.querySelector(".styles__title__ZrO6C").textContent.trim();
@@ -78,9 +78,68 @@ async function exportToCSV(filename, csvData) {
   'Entrants', 'Fill%', 'slate', 'Max Entries', 'Draft Size', 'Rounds', 'Rake', 'start_time']];
 
   async function live_exp() {
+    // var slateDivs = document.querySelectorAll(".styles__liveSlateHeaderCell__zdCK5");
+    // var slate = "NBA Fri 6:30PM"
+    // var targetDiv = null;
+
+    // slateDivs.forEach(function(div) {
+    //     var paragraphs = div.querySelectorAll("p");
+    
+    //     paragraphs.forEach(function(paragraph) {
+    //         if (paragraph.textContent.includes(slate)) {
+    //             targetDiv = div;
+    //             return;
+    //         }
+    //     });
+        
+    //     if (targetDiv) {
+    //         return;
+    //     }
+    // });
+
+    // targetDiv.click();
+    // await sleep(500);
+
+    // if (targetDiv) {
+    //     // Get all div elements in the document
+    //     var allDivs = document.querySelectorAll("div");
+    
+    //     // Initialize an array to store divs with the specified class after targetDiv
+    //     var tourneys = [];
+    
+    //     // Variable to track whether targetDiv is found
+    //     var foundTargetDiv = false;
+    
+    //     // Iterate through all div elements
+    //     allDivs.forEach(function(div) {
+    //         // Check if the current div is the targetDiv
+    //         if (div === targetDiv) {
+    //             foundTargetDiv = true;
+    //         } else if (foundTargetDiv) {
+    //             // If targetDiv has been found, check if the current div has the specified class
+    //             if (div.classList.contains("styles__draftPoolContent__OvOLG")) {
+    //                 // If it has the class, add it to the result array
+    //                 tourneys.push(div);
+    //             }
+    //         }
+    //     });
+    
+    //     // Now divsWithClassAfterTargetDiv contains all divs with the specified class 
+    //     // that appear after targetDiv in the HTML structure
+    //     console.log(tourneys);
+    // }
     var tourneys = Array.from(document.querySelectorAll(".styles__draftPoolContent__OvOLG"));
+    // console.log(tourneys_old);
+
     if (tourneys.length > 0) {
         for (let tourneyIndex = 0; tourneyIndex < tourneys.length; tourneyIndex++) {
+            // if (tourneyIndex > 0) {
+            //     console.log("True");
+            //     // await sleep(500);
+            //     // await targetDiv.click();
+            //     await sleep(500);
+            // }
+            // await sleep(1000);
             var tourneys = Array.from(document.querySelectorAll(".styles__draftPoolContent__OvOLG"));
             await sleep(1000);
             tourneys[tourneyIndex].click();
